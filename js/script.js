cf58621870eb1b40efc922b0abf4bf925650dc05
PETKAPIPA <narlor200@gmail.com>
2024-07-24 18:29:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let item = document.querySelector(".item");
+
+block.onmousemove = function (e) {
+  item.style.position = "fixed";
+  item.style.left = e.clientX + -20 + "px";
+  item.style.top = e.clientY + -20 + "px";
+};
+
+const peta = '../images/petia.png'
+const fil = '../images/filipp.png'
+const miron = '../images/miron.png'
+const ivan = '../images/vania.png'
+
+
+function changeImage(imgsrc) {
+  let myImg = document.getElementById('myImg');
+  myImg.src = imgsrc;
+};
+
+
